1381f4ffe408864ca9783da77eed8d7b3c97a41a
Randy Secrist <randy.secrist@gmail.com>
2012-02-09 16:19:59 -0700

    Added MSH test case.

---
+++ test/test_msh_segment.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require 'ruby-hl7'
+
+class MshSegment < Test::Unit::TestCase
+  def setup
+    @base_msh = 'MSH|^~\&|STAR|1154302487|TESTAPP|TESTHOSP|201105010200||ADT^A01|56364182|D|2.3|56364182||AL'
+  end
+
+  def test_create_msh
+    assert_nothing_raised do
+      msh = HL7::Message::Segment::MSH.new @base_msh
+      assert_not_nil msh
+      assert_equal @base_msh, msh.to_s
+    end
+  end
+
+  def test_access_msh
+    assert_nothing_raised do
+      msh = HL7::Message::Segment::MSH.new @base_msh
+      assert_equal '56364182', msh.message_control_id
+      assert_equal 'ADT^A01', msh.message_type
+    end
+  end
+
+end
